9d0dea82811c38876f83d59191719e8e983c3bbf
RENAN029 <pires.s@escolar.ifrn.edu.br>
2026-01-08 16:40:10 -0300

    Update c.sh

--- c.sh
+++ c.sh
@@ -7,24 +7,6 @@ sudo pacman -S --noconfirm gdu
 
 nvidia-open intel-ucode neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 
  
-echo "sudo pacman -S fzf fish tealdeer ripgrep zoxide eza bat fd lazygit lazydocker" 
-sudo pacman -S --noconfirm gamemode fuse dnsmasq apparmor 
-git clone https://github.com/maximilionus/lucidglyph.git
-systemctl enable cosmic-greeter earlyoom dnsmasq apparmor 
-cd lucidglyph
-sudo ./lucidglyph.sh install
-cd ..
-rm -rf lucidglyph
-
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst"
-sudo pacman -U --noconfirm "https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst"
-sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
-sudo sed -i '/Color/a ILoveCandy' /etc/pacman.conf
-sudo sed -i '/^ParallelDownloads/d' /etc/pacman.conf
-sudo sed -i '/ILoveCandy/a ParallelDownloads = 15' /etc/pacman.conf
-echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
 sudo pacman -Syu --noconfirm 
 sudo pacman -Scc --noconfirm
